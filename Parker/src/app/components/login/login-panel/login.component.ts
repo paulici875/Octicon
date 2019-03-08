8fd858ad8d8ec176f1704694df562a5c15b7b933
Paul <Paul>
2019-03-08 19:12:21 +0200

    small changes

--- Parker/src/app/components/login/login-panel/login.component.ts
+++ Parker/src/app/components/login/login-panel/login.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'logint',
+  selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
 
   constructor() { }
 
