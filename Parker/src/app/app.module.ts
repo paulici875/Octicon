c6eb9c7e74863168e2f1934e57d7fa43ee9533bc
bledeabogdan <bogdan.bledea@evozon.com>
2019-03-14 17:25:01 +0200

    - card component

--- Parker/src/app/app.module.ts
+++ Parker/src/app/app.module.ts
@@ -5,15 +5,20 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login-panel/login.component';
+import { DashboardComponent } from './containers/dashboard/dashboard.component';
+import { CardComponent } from './components/card/card.component';
 
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    DashboardComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
